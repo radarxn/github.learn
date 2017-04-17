2bd541d9369ba7f638d9e6cc6fac6edbe38f34f9
赵巍 <@Radar>
2017-04-17 10:05:13 +0800

    Signed-off-by: 赵巍 <赵巍@Radar>

---
+++ githublearn/src/main/java/myalice_websocket/githublearn/App.java
@@ -0,0 +1,13 @@
+package myalice_websocket.githublearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
